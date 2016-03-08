415822b273152c90e5d5f38d0fdf507a88a4424b
lijinlong <1187483952@qq.com>
2016-03-08 18:59:38 +0800

    添加代码

---
+++ src/main/java/cn/sinobest/flume/client/demo/LogDemo.java
@@ -0,0 +1,12 @@
+package cn.sinobest.flume.client.demo;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class LogDemo {
+	static final Log log = LogFactory.getLog(LogDemo.class);
+	
+	public static void main(String[] args) {
+		log.info("{'name':'Adam', 'age':'26', 'skill':'reading'}");
+	}
+}
